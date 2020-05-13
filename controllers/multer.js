f2c5a5b69d4eaaa054b732dc1c18386e097e48e4
psycoder01 <aakashchy13@gmail.com>
2020-05-13 20:50:36 +0545

    added image extension checkup

--- controllers/multer.js
+++ controllers/multer.js
@@ -1,4 +1,5 @@
 const multer = require("multer");
+const path = require("path");
 
 const storage = multer.diskStorage({
   destination: "./public/uploads",
@@ -10,6 +11,27 @@ const storage = multer.diskStorage({
   },
 });
 
-const upload = multer({ storage: storage }).single("avatar");
+const upload = multer({
+  storage: storage,
+  limits: { fileSize: 1000000 },
+  fileFilter: function (req, file, cb) {
+    checkFileTypes(file, cb);
+  },
+}).single("avatar");
+
+function checkFileTypes(file, cb) {
+  //Allowed Extesions
+  const fileTypes = /jpg|jpeg|png|gif/;
+
+  //File Extension Checkup and Mime checkup
+  const ext = fileTypes.test(path.extname(file.originalname).toLowerCase());
+  const mime = fileTypes.test(file.mimetype);
+
+  if (mime && ext) {
+    return cb(null, true);
+  } else {
+    return cb("Error: Not a valid Image file");
+  }
+}
 
 module.exports = upload;
--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -2,6 +2,7 @@ const upload = require("./multer");
 
 //Image upload controller
 const imageUpload = (req, res) => {
+
   upload(req, res, (err) => {
     if (err) return res.status(400).send("Error Uploading Image");
     res.json({
